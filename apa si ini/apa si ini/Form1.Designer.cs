3ac27bd2ec5e91d84536025951ccdef0e3edb061
tfilushandy <127372146+tfilushandy@users.noreply.github.com>
2023-04-13 13:13:13 +0700

    Add files via upload

---
+++ apa si ini/apa si ini/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace apa_si_ini
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_pemain = new System.Windows.Forms.DataGridView();
+            this.lbltext = new System.Windows.Forms.Label();
+            this.combobox_nationality = new System.Windows.Forms.ComboBox();
+            this.comboboxteam = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_pemain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_pemain
+            // 
+            this.dgv_pemain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_pemain.Location = new System.Drawing.Point(13, 140);
+            this.dgv_pemain.Name = "dgv_pemain";
+            this.dgv_pemain.Size = new System.Drawing.Size(775, 256);
+            this.dgv_pemain.TabIndex = 0;
+            // 
+            // lbltext
+            // 
+            this.lbltext.AutoSize = true;
+            this.lbltext.Location = new System.Drawing.Point(12, 66);
+            this.lbltext.Name = "lbltext";
+            this.lbltext.Size = new System.Drawing.Size(35, 13);
+            this.lbltext.TabIndex = 1;
+            this.lbltext.Text = "label1";
+            // 
+            // combobox_nationality
+            // 
+            this.combobox_nationality.FormattingEnabled = true;
+            this.combobox_nationality.Location = new System.Drawing.Point(13, 32);
+            this.combobox_nationality.Name = "combobox_nationality";
+            this.combobox_nationality.Size = new System.Drawing.Size(121, 21);
+            this.combobox_nationality.TabIndex = 2;
+            this.combobox_nationality.SelectedValueChanged += new System.EventHandler(this.combobox_nationality_SelectedValueChanged);
+            // 
+            // comboboxteam
+            // 
+            this.comboboxteam.FormattingEnabled = true;
+            this.comboboxteam.Location = new System.Drawing.Point(627, 32);
+            this.comboboxteam.Name = "comboboxteam";
+            this.comboboxteam.Size = new System.Drawing.Size(161, 21);
+            this.comboboxteam.TabIndex = 3;
+            this.comboboxteam.SelectedValueChanged += new System.EventHandler(this.comboboxteam_SelectedValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboboxteam);
+            this.Controls.Add(this.combobox_nationality);
+            this.Controls.Add(this.lbltext);
+            this.Controls.Add(this.dgv_pemain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_pemain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_pemain;
+        private System.Windows.Forms.Label lbltext;
+        private System.Windows.Forms.ComboBox combobox_nationality;
+        private System.Windows.Forms.ComboBox comboboxteam;
+    }
+}
+
---
+++ apa si ini/apa si ini/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace apa_si_ini
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        MySqlCommand cmd;
+        MySqlDataAdapter adapter;
+        string connectionString;
+        string sqlQuery;
+        DataTable dtpemain = new DataTable();
+        DataTable dtnationality = new DataTable();
+        DataTable dtteam = new DataTable();
+        DataTable dtplayerteam = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectionString = "server=localhost;uid=root;pwd=;database=premier_league";
+            conn = new MySqlConnection(connectionString);
+            conn.Open(); // ketika kalian menggunakan DML
+            //sqlQuery = "select * from player;";
+            //cmd = new MySqlCommand(sqlQuery, conn);
+            //adapter = new MySqlDataAdapter(cmd);
+            //adapter.Fill(dtpemain);
+            //dgv_pemain.DataSource = dtpemain;
+            //// 
+            //sqlQuery = "Select abv as 'Abrevation', nation as 'Nation' " + "FROM nationality;";
+            //cmd = new MySqlCommand(sqlQuery, conn);
+            //adapter = new MySqlDataAdapter(cmd);
+            //adapter.Fill(dtnationality);
+            //combobox_nationality.DataSource = dtnationality;
+            //combobox_nationality.ValueMember = "Abrevation";
+            //combobox_nationality.DisplayMember = "Nation";
+            ////
+            sqlQuery = "SELECT team_name as 'Team Name',team_id as 'Team ID' FROM team t;";
+            cmd = new MySqlCommand(sqlQuery, conn);
+            adapter = new MySqlDataAdapter(cmd);
+            adapter.Fill(dtteam);
+            comboboxteam.DataSource = dtteam;
+            comboboxteam.ValueMember = "Team ID";
+            comboboxteam.DisplayMember = "Team Name";
+            sqlQuery = "SELECT player_id , player_name , height , team_name FROM player INNER JOIN team ON player.team_id = team.team_id";
+            cmd = new MySqlCommand(sqlQuery, conn);
+            adapter = new MySqlDataAdapter(cmd);
+            adapter.Fill(dtplayerteam);
+            dgv_pemain.DataSource = dtplayerteam;
+            
+        }
+
+        private void combobox_nationality_SelectedValueChanged(object sender, EventArgs e)
+        {
+            lbltext.Text = combobox_nationality.SelectedValue.ToString();
+        }
+
+        private void comboboxteam_SelectedValueChanged(object sender, EventArgs e)
+        {
+            dtplayerteam.Clear();
+            dgv_pemain.DataSource = null;
+
+            sqlQuery = $"SELECT player_id , player_name , height , team_name FROM player INNER JOIN team ON player.team_id = team.team_id WHERE player.team_id = '{comboboxteam.SelectedValue.ToString()}'";
+            cmd = new MySqlCommand(sqlQuery, conn);
+            adapter = new MySqlDataAdapter(cmd);
+            adapter.Fill(dtplayerteam);
+            dgv_pemain.DataSource = dtplayerteam;
+        }
+    }
+}
